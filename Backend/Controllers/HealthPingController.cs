4c4ccdc838103814cf73166e616f329e8107a131
XiaoxuHU <xiaoxhu@microsoft.com>
2021-06-23 10:28:25 -0700

    Add Health Ping for backend project

---
+++ Backend/Controllers/HealthPingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api")]
+    public class HealthPingController : Controller
+    {
+        [HttpGet("healthping")]
+        public IActionResult HealthPing()
+        {
+            return Ok("App is running");
+        }
+    }
+}
